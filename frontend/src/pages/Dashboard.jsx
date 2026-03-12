868dc371d903de860bc110e675d452d0a21fcf4e
xxemat20xx <62041802+xxemat20xx@users.noreply.github.com>
2026-03-12 21:06:28 +0800

    commit changes

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-
+import { useAuthStore } from "../store/authStore"
 const Dashboard = () => {
+  const { logout } = useAuthStore();
+
   return (
-    <div className='text-6xl text-black'>Dashboard</div>
+    <>
+    <nav className='fixed top-0 bg-amber-600 text w-full py-3 px-12 flex items-center justify-between'>
+      <a href="#" className='font-bold text-lg text-white'>Navbar</a>
+
+      <div className='flex space-x gap-3'>
+        <button>Test</button>
+        <button onClick={() => logout()}>Logout</button>
+      </div>
+    </nav>
+    <div className='text-6xl text-black mt-24 px-12'>Dashboard</div>
+  
+    </>
+    
   )
 }
 
